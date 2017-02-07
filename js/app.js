90678d8371ce1ff44ee2753e6a2798a40dbf31b1
Rubi Cabral <rubisc@gmail.com>
2017-02-06 20:18:40 -0800

    input and guess button exist, but not finalized

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 console.log("Connection confirmed!")
-// STRINGS TO GUESS
+// GLOBAL VARIABLES-STRINGS TO GUESS
 // Movie Lines Category
 var movieLines = ["Say hello to my little friend", "May the force be with you", "Here's looking at you, kid", "Expecto Patronum!", "You can't sit with us", "Bond, James Bond", "I'm the king of the world!", "Why is the rum gone?", "There's no place like home", "Hakuna Matata"]
 // Movie/TV Titles Category
@@ -8,19 +8,24 @@ var movieTvTitles = ["Fight Club", "The Matrix", "Titanic", "Jurassic Park", "Jo
 var movieTvChar = ["Batman", "Harvey Specter", "Black Mamba", "Hermione Granger", "Vesper Lynd", "Rory Gilmore", "Tyler Durden", "Katniss Everdeen", "Sheldon Cooper", "Lagertha Lothbrok"]
 // Travel Spots Category
 var travelSpots = ["Taj Mahal", "Chichen Itza", "Great Wall of China", "Colosseum", "Eiffel Tower", "Teotihuacan Pyramids", "Times Square", "Golden Gate Bridge", "Machu Picchu", "The White House"]
-// Used in order to randomly pick a one of the phrases from each array (this variable's value is reassigned within each category's function)
+// to set up play page:
 function startPlayPage () {
   $('.button').hide()
   $('h2').hide()
   $('p').hide()
   $('#gallows-goes-here').html('<img src="./assets/hangman0.png">')
+  //THIS WON'T WORK! $('#gallows-goes-here').append({
+  //   'input': 'guess-letter',
+  //   'button': 'submit-guess'
+  // })
+
 }
 // To randomly select a string from the indicated array:
 function renderMovieLines() {
   var randomlyPick = movieLines.splice(Math.floor(Math.random() * movieLines.length), 1);
   console.log(randomlyPick)
 }
-// Upon clicking the indicated  button, randomly select a phrase and hide contents of page:
+
 $('#movie-line-button').on('click', function() {
   renderMovieLines()
   startPlayPage()
@@ -29,32 +34,31 @@ $('#movie-line-button').on('click', function() {
 // To randomly select a string from the indicated array:
 function renderMovieTvTitles() {
   randomlyPick = movieTvTitles.splice(Math.floor(Math.random() * movieTvTitles.length), 1);
-  // console.log(movieTvTitles)
   console.log(randomlyPick)
 }
-// Upon clicking the indicated  button, randomly select a phrase and hide contents of page:
+
 $('#movietvtitle-button').on('click', function() {
   renderMovieTvTitles()
   startPlayPage()
 })
-//
+
 // To randomly select a string from the indicated array:
 function renderMovieTvChar() {
   randomlyPick = movieTvChar.splice(Math.floor(Math.random() * movieTvChar.length), 1);
   console.log(randomlyPick)
 }
-// Upon clicking the indicated  button, randomly select a phrase and hide contents of page:
+
 $('#movietv-char-button').on('click', function() {
   renderMovieTvChar()
   startPlayPage()
 })
-//
+
 // To randomly select a string from the indicated array:
 function renderTravelSpots() {
   randomlyPick = travelSpots.splice(Math.floor(Math.random() * travelSpots.length), 1);
   console.log(randomlyPick)
 }
-// Upon clicking the indicated  button, randomly select a phrase and hide contents of page:
+
 $('#travel-button').on('click', function() {
   renderTravelSpots()
   startPlayPage()
